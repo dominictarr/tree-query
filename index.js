8407e1f9e58eab4018b58e6f52a705383952338d
Dominic Tarr <dominic.tarr@gmail.com>
2013-05-14 10:08:40 +0100

    add a few more output styles

--- index.js
+++ index.js
@@ -4,8 +4,40 @@ var pfs  = require('pull-fs')
 var glob = require('pull-glob')
 var chainable = require('./chainable')
 
+var fs = require('fs')
+
+function log () {
+  return pull.drain(console.log)
+}
+
+var simple = {
+  log: log,
+  stdout: log,
+  json: function (name, vName) {
+    var first = true
+    return pull.drain(function (e) {
+      if(!first)
+        console.log(',')
+      else {
+        console.log(name ? '{' : '[')
+      }
+
+      first = false
+      var value = JSON.stringify(vName ? e[vName] : e, null, 2)
+      console.log(name ? JSON.stringify(e[name]) +': ' + value : value)
+    }, function (err) {
+      console.log(']')
+    })
+  },
+  stat: function () {
+    return pull.asyncMap(function (f, cb) {
+      fs.stat(f, cb)
+    })
+  }
+}
+
 var methods = [
-  pull, pfs, {glob: glob},
+  pull, pfs, {glob: glob}, simple,
 ]
 
 var $ = module.exports = chainable(methods, function (x, y) {
