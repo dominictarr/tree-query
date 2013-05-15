f616b8f5c64d0ff0f15845ef63e614a82be1d803
Dominic Tarr <dominic.tarr@gmail.com>
2013-05-16 00:34:22 +0200

    fix example

--- example.js
+++ example.js
@@ -2,8 +2,8 @@ var $ = require('./')
 
 
 $(".../node_modules/*/package.json")
-.read(JSON.parse)
+.readFile(JSON.parse)
 .map(function (e) {return {name: e.name, version: e.version}})
 .unique('name')
-.collect(console.log)()
+.json()
 
